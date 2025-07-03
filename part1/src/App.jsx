007746013eb30c9c700be99c8b04ba16a1039255
kelvinfrancis <kelvinmoquete0597@gmail.com>
2025-07-03 15:03:07 -0400

    Refactor App component to remove counter functionality and implement click tracking

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -50,9 +50,9 @@ import { useState } from "react"
 //   )
 // }
 
-const Display = ({ counter }) => <div>{counter}</div>
+// const Display = ({ counter }) => <div>{counter}</div>
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+// const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
   // const course = {
@@ -74,30 +74,47 @@ const App = () => {
   // }
   // const name = 'Peter';
   // const age = 10
-  const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
+  // const [counter, setCounter] = useState(0)
+  // console.log('rendering with counter value', counter)
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-  const decreaseByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
+  // const increaseByOne = () => {
+  //   console.log('increasing, value before', counter)
+  //   setCounter(counter + 1)
+  // }
+  // const decreaseByOne = () => {
+  //   console.log('decreasing, value before', counter)
+  //   setCounter(counter - 1)
+  // }
+  // const setToZero = () => {
+  //   console.log('resetting to zero, value before', counter)
+  //   setCounter(0)
+  // }
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0
+  })
+
+  const handleLeftClick = () => setClicks({
+    ...clicks,
+    left: clicks.left + 1
+  })
+
+  const handleRightClick = () => setClicks({
+    ...clicks,
+    right: clicks.right + 1
+  })
 
   return (
     <div>
-
-      <Display counter={counter} />
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+      {/* <Display counter={counter} />
       <br />
       <Button onClick={increaseByOne} text='plus' />
       <Button onClick={decreaseByOne} text='minus' />
-      <Button onClick={setToZero} text='zero' />
+      <Button onClick={setToZero} text='zero' /> */}
       {/* <Header course={course} />
       <Content parts={course.parts} />
       <Total parts={course.parts} /> */}
