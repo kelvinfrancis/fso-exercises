f53931471c2dccaa86992684d40cf0cb47c7f62a
kelvinfrancis <kelvinmoquete0597@gmail.com>
2025-07-14 12:31:32 -0400

    Refactor SumExercises to use reduce for calculating total exercises

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -15,10 +15,13 @@ const Part = ({part}) => (
 );
 
 const SumExercises = ({parts}) => {
-  let sum = 0
-  parts.map((part) => (sum += part.exercises))
+  const initialValue = 0
+  const sumExercises = parts.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.exercises, initialValue
+  );
+
   return(
-    <p><strong>Total of {sum} exercises</strong></p>
+    <p><strong>Total of {sumExercises} exercises</strong></p>
   )
 }
 
