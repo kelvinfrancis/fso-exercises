76ec2e51a88826fa8851533a3444bdf0c06ffc89
kelvinfrancis <kelvinmoquete0597@gmail.com>
2025-07-17 12:10:57 -0400

    phonebook ejercicio 1 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,19 +6,34 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({
+      name: newName
+    }));
+  }
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input type='text' onChange={handlePersonChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </div>
     </div>
   )
 }
